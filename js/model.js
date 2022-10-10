9b2c53a0d623be4ad1b9d3726c8f88cd158fd549
Phat Tu <77705393+sturekev@users.noreply.github.com>
2022-10-09 19:14:51 -0500

    task due by 10/09/2022 for Bill and Kevin

--- js/model.js
+++ js/model.js
@@ -1,1 +1,18 @@
 // model relocate and save data set from api and use for views to updata interface
+
+// class cho từng anime 
+
+class Anime {
+    constructor(Name, Status, urlLink, Title, AlternateTitle, episodeCount, coverImg, Description, Type, Genres) {
+        this._animeName = Name;
+        this._animeStatus = Status;
+        this._animeUrl = urlLink;
+        this._animeTitle = Title;
+        this._animeAlterTitle = AlternateTitle;
+        this._animeEpisode = episodeCount;
+        this._animeCoverImg = coverImg;
+        this._animeDescript = Description;
+        this._animeType = Type;
+        this._animeGenres = Genres;
+    }
+}
