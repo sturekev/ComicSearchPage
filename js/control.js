1f5a742efda6c91ba0a310ceb20d8e5ab4b60742
Phat Tu <77705393+sturekev@users.noreply.github.com>
2022-10-11 11:29:41 -0500

    update controll

--- js/control.js
+++ js/control.js
@@ -11,17 +11,32 @@
 // 'X-RapidAPI-Host': 'jikan1.p.rapidapi.com'
 // 
 //populate the info and choice for 
-const options = {
+
+// call model
+var animeModel = new allAnime();
+var mangaModel = new allManga();
+
+// api Jikan 
+const jikanApi = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'c7bdf33b7dmshcf42308cd22add9p16fdb8jsnf56785f70313',
-		'X-RapidAPI-Host': 'animi.p.rapidapi.com'
+		'X-RapidAPI-Host': 'jikan1.p.rapidapi.com'
 	}
 };
-
-fetch('https://animi.p.rapidapi.com/name?name=Levi', options)
+data  = await fetch('https://jikan1.p.rapidapi.com/top/anime/1/upcoming', jikanApi)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+//
+async function populateSugestion(type){
+	topAnime  = await fetch(`https://jikan1.p.rapidapi.com/top/${type}`, jikanApi)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+}
 
-console.log(options);
+window.onload = function(){
+	let sugestionId = document.querySelector("suggestionContent");
+	populateSugestion(jikanApi);
+};
--- js/views.js
+++ js/views.js
@@ -1,2 +1,9 @@
 // show ouw page interface
 // same methods as list of stuff 
+
+// cần suy nghĩ thêm
+class pageView {
+    constructor(model){
+        model.subscribe(this.redrawTable.bind(this))
+    }
+}
