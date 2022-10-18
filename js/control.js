14d5191cfa5f61ca36b9f1f2a48245eb4cb70d2c
bhy2001 <88808104+bhy2001@users.noreply.github.com>
2022-10-17 19:46:01 -0500

    Hy Dang, update index.html and added suggestion functions

--- js/control.js
+++ js/control.js
@@ -9,54 +9,67 @@
 //api -- https://rapidapi.com/theapiguy/api/jikan1/
 // 'X-RapidAPI-Key': 'c7bdf33b7dmshcf42308cd22add9p16fdb8jsnf56785f70313',
 // 'X-RapidAPI-Host': 'jikan1.p.rapidapi.com'
-// 
-//populate the info and choice for 
+//
+//populate the info and choice for
 
 // call model
 var animeModel = new allAnime();
 var mangaModel = new allManga();
 var pageViews = new pageView();
 var searchModel = new Search();
-// api Jikan 
-const jikanApi = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c7bdf33b7dmshcf42308cd22add9p16fdb8jsnf56785f70313',
-		'X-RapidAPI-Host': 'jikan1.p.rapidapi.com'
-	}
-};
+// api Jikan
+// const jikanApi = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "c7bdf33b7dmshcf42308cd22add9p16fdb8jsnf56785f70313",
+//     "X-RapidAPI-Host": "jikan1.p.rapidapi.com",
+//   },
+// };
 //
-async function populateSugestion(type){
-	top  = await fetch(`https://jikan1.p.rapidapi.com/top/${type}`, jikanApi)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+async function populateSugestion(type) {
+  top = await fetch(`https://api.jikan.moe/v4/top/${type}`)
+    .then((response) => response.json())
+    .then((response) => {
+      // console.log(response['data'][0]['title']);
+      let table = document.querySelector("#suggest-table > tbody");
+      table.innerHTML = "";
+      for (let i = 0; i < 5; i++) {
+        let titl = response["data"][i]["title"];
+        let row = document.createElement("tr");
+        let title_number = document.createElement("td");
+        title_number.innerHTML = `<b>${(i + 1).toString()}</b>`;
+        let title_data = document.createElement("td");
+        title_data.innerHTML = titl.toString();
+        row.appendChild(title_number);
+        row.appendChild(title_data);
+        table.appendChild(row);
+      }
+    })
+    .catch((err) => console.error(err));
 }
 
-function saveWatchList (){
-	localStorage.setItem("local_AnimeWatchList",JSON.stringify(animeModel));
-	localStorage.setItem("local_MangaWatchList",JSON.stringify(mangaModel));
+function saveWatchList() {
+  localStorage.setItem("local_AnimeWatchList", JSON.stringify(animeModel));
+  localStorage.setItem("local_MangaWatchList", JSON.stringify(mangaModel));
 }
+
 var deletePlayerIdList = [];
-function clearWatchList (){ 
-    let cbList = document.querySelectorAll("#watch-table > tbody > tr > td > input[type = 'checkbox']");
-    for (let checkbox of cbList){
-        if (checkbox.checked){
-            deletePlayerIdList.push(parseInt(checkbox.closest('tr')['id']));
-        }
+function clearWatchList() {
+  let cbList = document.querySelectorAll(
+    "#watch-table > tbody > tr > td > input[type = 'checkbox']"
+  );
+  for (let checkbox of cbList) {
+    if (checkbox.checked) {
+      deletePlayerIdList.push(parseInt(checkbox.closest("tr")["id"]));
     }
-    searchModel.removeSearch(deletePlayerIdList);
+  }
+  searchModel.removeSearch(deletePlayerIdList);
 }
 
-function RemoveAll(){
-	animeModel.RemoveAll()
-	mangaModel.RemoveAll()
+function RemoveAll() {
+  animeModel.RemoveAll();
+  mangaModel.RemoveAll();
 }
-function watchList (model){
-	
-}
+function watchList(model) {}
 
-window.onload = function(){
-	let sugestionId = document.querySelector("suggestionContent");
-	populateSugestion(jikanApi);
-};
+window.onload = function () {};
