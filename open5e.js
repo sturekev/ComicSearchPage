eaf0f58282803265ade9fa5074332565c318816d
Phat Tu <77705393+sturekev@users.noreply.github.com>
2022-10-20 13:54:56 -0500

    delete log

--- open5e.js
+++
@@ -1,8 +0,0 @@
-let search_res = await fetch(
-    `https://api.open5e.com/monsters/?search=fir`
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => console.error(err));
